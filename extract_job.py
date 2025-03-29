d76d694e1594e65dbaa636d48daf201888014abc
Shrey7966 <94635069+Shrey7966@users.noreply.github.com>
2025-03-29 19:35:47 +0530

    Create extract_job.py

---
+++ extract_job.py
@@ -0,0 +1,40 @@
+import requests
+import pandas as pd
+import boto3
+import os
+from datetime import datetime
+
+# Load environment variables
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+S3_BUCKET = os.getenv("S3_BUCKET_NAME")
+
+#Step-1 Call Flights API 
+url = "https://flights-sky.p.rapidapi.com/flights/search-one-way"
+
+querystring = {"fromEntityId":"BLR",
+               "toEntityId":"DFW",
+               "departDate":"2025-05-31",
+               "market":"IN",
+               "locale":"en-US",
+               "currency":"INR",
+               "stops":"direct,1stop,2stops",
+               "cabinClass":"economy",
+               "adults":"1",
+               "sort":"cheapest_first"}
+
+headers = {
+	"x-rapidapi-key": "fdcf5105b0mshb60125cb25ee57ep1565acjsndd740fc93e14",
+	"x-rapidapi-host": "flights-sky.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+#Step-2 Store the response in s3 bucket
+
+s3_key = f"flight_prices/{fetch_date}/{depart_date}.json" # New path format
+    
+s3 = boto3.client("s3")
+s3.put_object(Bucket=S3_BUCKET, Key=s3_key, Body=json.dumps(data))
+    
+print(f"Saved data to s3://{s3_bucket}/{s3_key}")
